91c8b4f183abdff99d876697cf0629be6d4f4a46
OldFreddy <oldfredddy@gmail.com>
2019-08-27 19:15:26 +0300

    7) Условные операторы if /else if/ else

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,27 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	    System.out.println("Hello World!");
+        Scanner num = new Scanner(System.in);
+        int first, second = 50;
+        System.out.println("Enter first num: ");
+        first = num.nextInt();
+
+        if (first >= second)
+        {
+            System.out.print("Num is 10");
+            System.out.print("\n");
+        } else if (first == 45)
+        {
+            System.out.print("Num is equal 45");
+
+        }
+        else
+        {
+            System.out.print("Num is lower than " + second);
+        }
     }
 }
