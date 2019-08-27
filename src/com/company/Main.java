b4375dfd436eda733dc299fb02312870be821b3c
OldFreddy <oldfredddy@gmail.com>
2019-08-27 19:35:24 +0300

    7) Условные операторы Краткая запись if

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,20 +7,19 @@ public class Main {
     public static void main(String[] args) {
         Scanner num = new Scanner(System.in);
         int first, second = 50;
+        boolean isTrue = true;
         System.out.println("Enter first num: ");
         first = num.nextInt();
 
-        if (first >= second)
-        {
+        isTrue = first == 51? true : false; // Если first = 51, то isTrue = first
+
+        if (first >= second && isTrue) {
             System.out.print("Num is 10");
             System.out.print("\n");
-        } else if (first == 45)
-        {
+        } else if (first == 45) {
             System.out.print("Num is equal 45");
-
         }
-        else
-        {
+        else {
             System.out.print("Num is lower than " + second);
         }
     }
