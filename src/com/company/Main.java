8cfbfa87c7cea241dde7dc4b5f079f99ebee6f0f
OldFreddy <oldfredddy@gmail.com>
2019-09-10 21:15:00 +0300

    11) Функции

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,12 +3,40 @@ package com.company;
 import java.awt.*;
 import java.util.Scanner;
 
+
 public class Main {
 
     public static void main(String[] args) {
-        char[][] array = {{1,2,3,6},{4,5,6},{7,8,9}};
-        array[0][0] = 'C';
-        System.out.println(array[2].length);
-        System.out.println(array[0][0]);
+      int temp = func (4 , 2);
+      System.out.print(temp);
+      int[] arr = readArray();
+      for (int i = 0; i<arr.length; i++){
+          System.out.print(arr[i] + " " );
+      }
+
+
     }
+    public static int[] readArray(){
+        int[] arr;
+        int n;
+        Scanner scan = new Scanner(System.in);
+        n = scan.nextInt();
+        arr = new int[n];
+        for (int i = 0; i < arr.length; i++){
+            arr[i] = scan.nextInt();
+
+        }
+        return arr;
+
+    }
+
+    public static int func(int a, int b) {
+        int result = 1;
+        for (int i = 0; i < b; i++){
+            result *= a;
+        }
+        return result;
+    }
+
+
 }
