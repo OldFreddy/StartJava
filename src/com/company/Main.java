1548b11dbdad21bb871b66b39354c28ea6b6da82
OldFreddy <oldfredddy@gmail.com>
2019-09-12 22:20:02 +0300

    13) Создание классов

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,27 +6,13 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        int [][] array;
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Введите количество строк в массиве \n");
-        int n = scan.nextInt();
-        System.out.println("Введите количество столбцов в массиве \n");
-        int m = scan.nextInt();
-        array = new int[n][m];
-
-        for (int i = 0; i < array.length; i++){
-            for (int j = 0; j < array[i].length; j++){
-                System.out.println("Введите [" + i + "][" + j + "] элемент" );
-                array[i][j] = scan.nextInt();
-            }
+        Person vitya = new Person();
+        vitya.height = 120;
+        vitya.say("MISHA");
+        System.out.println(vitya.height);
+        Person vlad = new Person();
+        System.out.print(vlad.height);
         }
 
-        for (int i = 0; i < array.length; i++){
-            System.out.println();
-            for (int j = 0; j < array[i].length; j++){
-                System.out.print(array[i][j] + " | ");
-            }
-        }
 
-    }
 }
---
+++ src/com/company/Person.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Person {
+    int height = 180;
+
+    public void say (String name){
+        System.out.println("Hello, " + name);
+    }
+}
